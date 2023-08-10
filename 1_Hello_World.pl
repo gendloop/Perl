be126f3c15cf63c0501cf9c97ebb71183bc681a8
gendloop <gendloop@163.com>
2023-08-11 02:55:08 +0800

    feat: add `1_Hello_World`

---
+++ 1_Hello_World.pl
@@ -0,0 +1,24 @@
+# 单行注释
+
+
+=pod 多行注释
+print "Hello World!\n";
+print "Hello World!\n";
+print "Hello World!\n";
+print "Hellod World!\n";
+=cut
+
+
+# print
+print "Hello World!\n";
+
+
+# print 空格, 换行
+print "Hello
+    World!\nHello\n";
+
+
+print "Hello\n"; # 双引号
+print 'Hello\n'; # 单引号
+
+
